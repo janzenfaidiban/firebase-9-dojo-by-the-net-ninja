bf7da6d9c6695ae33768eae4e2f3d5171d63a6a2
Janzen Faidiban <janzenfaidiban@gmail.com>
2021-12-10 01:37:34 +0900

    Video 5, Getting Started with Firebase 9

---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+const path = require('path')
+
+module.exports = {
+    devtool: 'source-map ./src',
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    watch: true
+}
